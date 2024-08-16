01f17737764e6b778fb2f7a333c3b69708eb291e
JimcostDev <rjak727@gmail.com>
2024-08-16 13:18:43 +0200

    anexo de ejercicios basicos: hello, calc y input

---
+++ ejercicios/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var greeting string = "Hola a todos"
+	name := "Ronaldo Jiménez Acosta"
+	age := 25
+	fmt.Printf("%v, soy %v y tengo %v años.\n", greeting, name, age)
+}
---
+++ ejercicios/input.go
@@ -0,0 +1,20 @@
+// ejemplo de entrada de datos - Scan y Scanf
+package main
+
+import "fmt"
+
+func main() {
+
+	// Scanf - permite especificar el formato de entrada con mas detalle, en este caso se espera que el usuario ingrese la hora en formato HH:MM:SS
+	var hour, minute, second int
+	fmt.Println("HH:MM:SS? ")
+	fmt.Scanf("%d:%d:%d", &hour, &minute, &second)
+
+	// Scan - permite leer un valor de entrada
+	var user string
+	fmt.Println("Ingresa tu usuario:")
+	fmt.Scan(&user)
+
+	fmt.Printf("Hola %v, bienvenido!\n", user)
+	fmt.Printf("La hora ingresada es: %v:%v:%v\n", hour, minute, second)
+}
