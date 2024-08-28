414c7051514a41fe35a676f3a20e8f1039bb0315
JimcostDev <rjak727@gmail.com>
2024-08-28 19:28:27 +0200

    archivos organizados por paquetes para evitar conflictos (main)

---
+++ go-exercises/calculator/calc.go
@@ -0,0 +1,21 @@
+// operaciones aritmeticas basicas
+package main
+
+import "fmt"
+
+func main() {
+
+	num1 := 5
+	num2 := 8
+
+	addition := num1 + num2
+	substraction := num1 - num2
+	multiplication := num1 * num2
+	division := float32(num1) / float32(num2)
+
+	fmt.Printf("La suma de %v y %v es %v\n", num1, num2, addition)
+	fmt.Printf("La resta de %v y %v es %v\n", num1, num2, substraction)
+	fmt.Printf("La multiplicación de %v y %v es %v\n", num1, num2, multiplication)
+	fmt.Printf("La división de %v y %v es %v\n", num1, num2, division)
+
+}
