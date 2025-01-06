99bab3b54a060a1990a8aa9d1804d24dd702ae52
JimcostDev <rjak727@gmail.com>
2025-01-06 18:02:23 +0100

    vectores (arrays)

--- examples-book/06-arrays-slices/01-arrays.go
+++ examples-book/06-arrays-slices/01-arrays.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	// En Go, los vectores se tratan por valor
-	src := [5]int{1, 2, 3, 4, 5}
+	src := [...]int{1, 2, 3, 4, 5} // ... significa que el tamaño del vector se infiere del número de elementos
 	var dst [5]int
 
 	// Eso significa que, en asignaciones, se copian
 	dst = src
 	src[0] = 9
 
-	fmt.Printf("%v != %v", src, dst)
+	fmt.Printf("%v != %v \n", src, dst)
+	fmt.Println(reflect.TypeOf(dst))
+	fmt.Println(reflect.TypeOf(src))
+	fmt.Println(reflect.TypeOf(dst) == reflect.TypeOf(src)) // true
 }
