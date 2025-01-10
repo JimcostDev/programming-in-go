2ea1a51eef1a8b0d478cb9244dc6658b858aae0c
JimcostDev <rjak727@gmail.com>
2025-01-10 11:50:15 +0100

    vectores y porciones

--- examples-book/06-arrays-slices/05-argumentos-funciones.go
+++ examples-book/06-arrays-slices/05-argumentos-funciones.go
@@ -7,12 +7,15 @@ func Cero(vec [3]int, porc []int) {
 	if len(porc) > 0 {
 		porc[0] = 0
 	}
+	fmt.Println(vec) // [0 2 3]
 }
 
+// las porciones son tratadas por referencia, por lo que se pueden modificar en la función
+// los vectores son tratados por valor, por lo que no se pueden modificar en la función
 func main() {
-	v := [3]int{1, 2, 3}
-	p := []int{1, 2, 3}
+	v := [3]int{1, 2, 3} // vector
+	p := []int{1, 2, 3}  // porción
 	Cero(v, p)
-	fmt.Println("vector:", v)
-	fmt.Println("porción:", p)
+	fmt.Println("vector:", v)  // [1 2 3]
+	fmt.Println("porción:", p) // [0 2 3]
 }
