109bfc3671b1ee0fe2fabf7a9e81e1efbd5587dc
JimcostDev <rjak727@gmail.com>
2025-01-10 11:38:09 +0100

    recorriendo slices(porciones)

--- examples-book/06-arrays-slices/03-iteración.go
+++ examples-book/06-arrays-slices/03-iteración.go
@@ -5,11 +5,18 @@ import "fmt"
 func main() {
 	ciudades := []string{"Tokyo", "Lleida", "Paris", "Madrid"}
 
+	fmt.Println("Primera forma:")
 	for i := 0; i < len(ciudades); i++ {
 		fmt.Printf("[%d] %s\n", i, ciudades[i])
 	}
 
+	fmt.Println("Segunda forma:")
 	for i, ciudad := range ciudades {
 		fmt.Printf("[%d] %s\n", i, ciudad)
+	}
+
+	fmt.Println("Tercera forma:")
+	for _, ciudad := range ciudades {
+		fmt.Println(ciudad)
 	}
 }
